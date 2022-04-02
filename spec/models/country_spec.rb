78bd27f3ed245812c4c1c9f6a5552c62c43d7c82
MasudaAfrin <masudatuba@gmail.com>
2022-04-02 23:43:19 +0600

    test model of sector, country and campaigns

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a name' do
+    country = Country.new(name: nil)
+    expect(country).to_not be_valid
+
+    country.name = 'England'
+    expect(country).to be_valid
+  end
 end
--- spec/models/sector_spec.rb
+++ spec/models/sector_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Sector, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a name' do
+    sector = Country.new(name: nil)
+    expect(sector).to_not be_valid
+
+    sector.name = 'England'
+    expect(sector).to be_valid
+  end
 end
